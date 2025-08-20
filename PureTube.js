f5cae9f7fca4b4395317a169566c2bc00d4cfd77
reneederer <rene.ederer@protonmail.com>
2025-08-20 10:38:14 +0200

    -

--- PureTube.js
+++ PureTube.js
@@ -18,18 +18,32 @@
     'ytd-playlist-video-renderer',
     '#related', 'ytd-watch-next-secondary-results-renderer',
     'ytd-browse', 'ytd-two-column-browse-results-renderer',
-    'ytd-mini-guide-renderer', '#guide', '#header'
+    'ytd-mini-guide-renderer', '#guide', '#header', 'ytd-mini-guide-renderer'
+  ];
+
+  const USER_HIDE = [
+    'ytd-searchbox', 'yt-searchbox', '#search-form',
+    //'ytd-rich-item-renderer', 'ytd-grid-video-renderer', 'ytd-rich-grid-row',
+    //'ytd-video-renderer', 'ytd-compact-video-renderer',
+    //'ytd-reel-shelf-renderer', 'ytd-reel-video-renderer',
+    //'ytd-playlist-video-renderer',
+    //'#related', 'ytd-watch-next-secondary-results-renderer',
+    //'ytd-browse', 'ytd-two-column-browse-results-renderer',
+    'ytd-mini-guide-renderer', '#guide',
+    //'#header'
   ];
 
   const WATCH_HIDE = [
     'ytd-searchbox', 'yt-searchbox', '#search-form',
     '#related', 'ytd-watch-next-secondary-results-renderer',
-    '#comments', '#masthead-container',
+    //'#comments', '#masthead-container',
     'ytd-mini-guide-renderer', '#guide', '#header',
     'ytd-merch-shelf-renderer', 'ytd-reel-shelf-renderer',
     'ytd-video-secondary-info-renderer', 'ytd-expander',
-    'ytd-watch-metadata', 'ytd-sentiment-bar-renderer',
-    'ytd-browse', 'ytd-two-column-browse-results-renderer'
+    //'ytd-watch-metadata', 'ytd-sentiment-bar-renderer',
+    'ytd-browse', 'ytd-two-column-browse-results-renderer',
+    '.shortsLockupViewModelHostEndpoint', 'ytd-item-section-renderer',
+    'ytd-item-section-renderer', 'ytd-mini-guide-renderer'
   ];
 
   const style = document.createElement('style');
@@ -39,6 +53,8 @@
   function updateCSS() {
     if (location.href.includes("watch?v=")) {
       style.textContent = `${WATCH_HIDE.join(',\n')} { display: none !important; }`;
+    } else if (location.href.includes("/@")) {
+      style.textContent = `${USER_HIDE.join(',\n')} { display: none !important; }`;
     } else {
       style.textContent = `${GENERAL_HIDE.join(',\n')} { display: none !important; }`;
     }
